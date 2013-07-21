5b63d357df383051e7e0abb8128824a5c8df301f
Brad Wilcox <brad_wilcox@me.com>
2013-07-21 09:56:52 -0400

    initial commit

---
+++ crowdfunder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdfunder2::Application.config.secret_token = 'ba7762059ebf23b564820e4113f991d3d0e40d73b629b380bb656173eb4a160d39e588e8aaa375893a0fd713b90f84d3ae69f5afef80afd11f87af99054d380a'
---
+++ rainforest2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest2::Application.config.secret_token = '5afd094ff6f0b85dc9226292692e448f4127cd7e51c81489e3c1ee71081110d58f9026f3d8af9dad97b93dff89ef0b7ad7d1b2004e97c153ae939c5c9e3f740b'
